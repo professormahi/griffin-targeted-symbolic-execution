4bd87542a73c2621d98775dcbca2cbcc3338dd13
Mahdi Fooladgar <mahdi.fooladgar@yahoo.com>
2023-04-14 15:31:59 +0330

    Feat: Add execution time

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from exporters import export_requested_parameters
 from sol_utils import SolFile
+from utils import log
 from workspace import workspace
 
 if __name__ == '__main__':
@@ -7,8 +10,11 @@ if __name__ == '__main__':
 
     export_requested_parameters(contract)
 
+    start_time = datetime.utcnow().timestamp()
     cfg_path = contract.find_test_data
     if cfg_path is None:
         print("No path found from target to smart contract entrypoint")
     else:
         print(f"Sat inputs: {cfg_path.sat_inputs}")
+    exec_time = datetime.utcnow().timestamp() - start_time
+    log(f"Exec Time: {exec_time:.2}s", level="info")
