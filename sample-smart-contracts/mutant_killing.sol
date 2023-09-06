fbcd290cd9ae34b460a2d1f0851dbf48ec9e2ad7
Mahdi Fooladgar <mahdi.fooladgar@yahoo.com>
2023-09-06 23:58:58 +0330

    Feat: Add mutant killing test case

---
+++ sample-smart-contracts/mutant_killing.sol
@@ -0,0 +1,10 @@
+// @result [{'function': 'check', 'params': {'a': 0, 'b': 0, 'msg.sender': 194866884977453722427157977695504402620791005730, 'msg.value': 0}}]
+pragma solidity ^0.8.0;
+
+contract mutant_killing {
+    function check(uint8 a, uint8 b) public returns (bool) {
+        if (a >= b)  // Original: `a > b`
+            return true;  // @target !(a > b)
+        return false;
+    }
+}
