39dac5c416b51a8206300befe7ffc7e41ecd2507
Mahdi Fooladgar <mahdi.fooladgar@yahoo.com>
2023-09-08 17:09:24 +0330

    Fix the typo in multi_tx variables and make the threshold configurable

--- sample-smart-contracts/multi_tx_10.sol
+++ sample-smart-contracts/multi_tx_10.sol
@@ -3,17 +3,18 @@ pragma solidity ^0.8.0;
 
 contract multi_tx_10 {
     uint public counter = 0;
-    uint public maximum_bet = 0;
+    uint public maximum_bid = 0;
+    uint private threshold = 10;
 
-    function bet(uint value) public {
+    function bid(uint value) public {
         counter += 1;
-        if (value > maximum_bet)
-            maximum_bet = value;
+        if (value > maximum_bid)
+            maximum_bid = value;
     }
 
     function check() public returns (uint) {
-        if (counter == 10)
-            return maximum_bet;  // @target maximum_bet > 100
+        if (counter == threshold)
+            return maximum_bid;  // @target maximum_bid > 100
         return 0;
     }
 }
--- sample-smart-contracts/multi_tx_5.sol
+++ sample-smart-contracts/multi_tx_5.sol
@@ -1,19 +1,20 @@
-// @result [{'function': 'slitherConstructorVariables', 'params': {'msg.sender': 487167212443634306067894944238761006551977514325, 'msg.value': 0}}, {'function': 'bet', 'params': {'value': 0, 'msg.sender': 97433442488726861213578988847752201310395502865, 'msg.value': 0}}, {'function': 'bet', 'params': {'value': 0, 'msg.sender': 389733769954907444854315955391008805241582011460, 'msg.value': 0}}, {'function': 'bet', 'params': {'value': 0, 'msg.sender': 292300327466180583640736966543256603931186508595, 'msg.value': 0}}, {'function': 'bet', 'params': {'value': 0, 'msg.sender': 292300327466180583640736966543256603931186508595, 'msg.value': 0}}, {'function': 'bet', 'params': {'value': 101, 'msg.sender': 97433442488726861213578988847752201310395502865, 'msg.value': 0}}, {'function': 'check', 'params': {'msg.sender': 487167212443634306067894944238761006551977514325, 'msg.value': 0}}]
+// @result [{'function': 'slitherConstructorVariables', 'params': {'msg.sender': 194866884977453722427157977695504402620791005730, 'msg.value': 0}}, {'function': 'bid', 'params': {'value': 0, 'msg.sender': 292300327466180583640736966543256603931186508595, 'msg.value': 0}}, {'function': 'bid', 'params': {'value': 0, 'msg.sender': 389733769954907444854315955391008805241582011460, 'msg.value': 0}}, {'function': 'bid', 'params': {'value': 0, 'msg.sender': 194866884977453722427157977695504402620791005730, 'msg.value': 0}}, {'function': 'bid', 'params': {'value': 0, 'msg.sender': 194866884977453722427157977695504402620791005730, 'msg.value': 0}}, {'function': 'bid', 'params': {'value': 101, 'msg.sender': 389733769954907444854315955391008805241582011460, 'msg.value': 0}}, {'function': 'check', 'params': {'msg.sender': 389733769954907444854315955391008805241582011460, 'msg.value': 0}}]
 pragma solidity ^0.8.0;
 
 contract multi_tx_5 {
     uint public counter = 0;
-    uint public maximum_bet = 0;
+    uint public maximum_bid = 0;
+    uint private threshold = 5;
 
-    function bet(uint value) public {
+    function bid(uint value) public {
         counter += 1;
-        if (value > maximum_bet)
-            maximum_bet = value;
+        if (value > maximum_bid)
+            maximum_bid = value;
     }
 
     function check() public returns (uint) {
-        if (counter == 5)
-            return maximum_bet;  // @target maximum_bet > 100
+        if (counter == threshold)
+            return maximum_bid;  // @target maximum_bid > 100
         return 0;
     }
 }
