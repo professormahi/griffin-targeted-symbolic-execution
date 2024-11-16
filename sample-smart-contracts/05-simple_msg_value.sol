94ad9e8c54d5b0bbc8ad2f231a96afef5aa7c460
Mahdi Fooladgar <mahdi.fooladgar@yahoo.com>
2024-11-16 17:00:53 +0330

    Fix: change simple msg value results

--- sample-smart-contracts/05-simple_msg_value.sol
+++ sample-smart-contracts/05-simple_msg_value.sol
@@ -1,5 +1,5 @@
 // Reference: https://github.com/trailofbits/manticore/blob/master/examples/evm/simple_value_check.sol
-// @result [{'function': 'constructor', 'params': {'msg.sender': 389733769954907444854315955391008805241582011460, 'msg.value': 0}}, {'function': 'target', 'params': {'msg.sender': 97433442488726861213578988847752201310395502865, 'msg.value': 27}}]
+// @result [{'function': 'constructor', 'params': {'msg.sender': 97433442488726861213578988847752201310395502865, 'msg.value': 0}}, {'function': 'target', 'params': {'msg.sender': 97433442488726861213578988847752201310395502865, 'msg.value': 10}}]
 
 contract SimpleMsgValueCheck {
     event Log(string);
@@ -11,9 +11,9 @@ contract SimpleMsgValueCheck {
     }
 
     function target() payable public {
-        if (msg.value > 10) {
-            value = msg.value; // @target !(value >= 10)
-            emit Log("Value greater than 10");
+        if (msg.value >= 10) {
+            value = msg.value;
+            emit Log("Value greater than 10");  // @target (value >= 10) && !(value > 10)
         } else {
             value = msg.value;
             emit Log("Value less or equal than 10");
